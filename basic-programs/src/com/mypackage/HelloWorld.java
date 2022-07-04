9b2a8ed0a5b1ed1c3521795e22a7c7f577398b32
kiranhbhangale <kiranhbhangale@gmail.com>
2022-07-04 17:30:38 +0530

    HelloWorld
    HelloWorld Program

---
+++ basic-programs/src/com/mypackage/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.mypackage;
+
+
+/*
+ * This is a simple Java program.
+ */
+
+// A Java program begins with a call to main(). 
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Java Hello World");
+	}
+}
